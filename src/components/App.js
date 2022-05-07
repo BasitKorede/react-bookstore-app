cb81d9b57ded8e4882dbf2e916c1ce6c8f2bb4d7
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-05-07 18:32:13 +0100

    Fix linter error

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,16 @@ import Header from './header/Header';
 import Books from './books/Books';
 import Categories from './categories/Categories';
 
-const App = () => (
-  <Router >
-    <Header />
-    <Routes>
-      <Route path="/" exact component={Books} />
-      <Route path="/categories" exact component={Categories} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" exact component={Books} />
+        <Route path="/categories" exact component={Categories} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
