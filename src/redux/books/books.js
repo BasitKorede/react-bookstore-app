d6557f8fd58ff1893e681ed69eaec6707308feb3
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-05-20 21:51:11 +0100

    Add default book to book store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,6 +2,16 @@ const ADD_BOOK = 'library/books/ADD_BOOK';
 const REMOVE_BOOK = 'library/books/REMOVE_BOOK';
 
 const stateInit = [{
+  id: 1, title: 'The Alchemist', category: 'Fiction', author: 'Paulo Coelho',
+},
+{
+  id: 2, title: 'Rich Dad Poor Dad', category: 'Financial', author: ' Robert Kiyosaki and Sharon Lechter',
+},
+{
+  id: 3, title: 'The Way of the Peaceful Warrior', category: 'Biography', author: 'Dan Millman',
+},
+{
+  id: 4, title: 'Think and Grow Rich ', category: 'Financial', author: 'Napoleon Hill',
 }];
 
 export const addBook = (payload) => ({
