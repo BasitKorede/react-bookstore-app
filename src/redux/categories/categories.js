0122f579f3fc3a30ba4e30ba3d03cc2fa72ea09b
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-05-20 09:52:54 +0100

    Make further changes based on reviewer's request

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,24 +1,20 @@
-const CHECK_STATUS = './library/categories/CHECK_STATUS';
-
-const stateInit = {
-  type: 'action',
-  checked: true,
-  payload: 'Under construction',
-};
-
-export const checkStatus = (payload) => ({
-  type: CHECK_STATUS,
-  payload,
+const CATEGORIES_STATUS = 'CATEGORY_STATUS';
+export const caretgoriesStatusAction = (status) => ({
+  type: CATEGORIES_STATUS,
+  payload: {
+    status,
+  },
 });
-
-const catReducer = (state = stateInit, action) => {
+const catReducer = (state = [], action) => {
   switch (action.type) {
-    case CHECK_STATUS:
-      return action.payload;
-
+    case CATEGORIES_STATUS:
+      return [
+        {
+          status: 'Under construction',
+        },
+      ];
     default:
       return state;
   }
 };
-
 export default catReducer;
